caa4e0f77dc58542b02b4f2136556082a0be4fda
VEM27 <v.myachin2007@gmail.com>
2024-09-22 16:42:03 +0300

    Update show_meme.py

--- show_meme.py
+++ show_meme.py
@@ -1,7 +1,3 @@
 import os
 meme1 = "C:\\Users\\V\\Desktop\\sirius\\memes1\\meme1.jpg"
-meme2 = "C:\\Users\\V\\Desktop\\sirius\\memes1\\meme2.jpg"
-meme3 = "C:\\Users\\V\\Desktop\\sirius\\memes1\\meme3.jpg"
 os.startfile(meme1)
-os.startfile(meme2)
-os.startfile(meme3)
